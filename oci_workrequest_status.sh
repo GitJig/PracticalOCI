13ccc46469e63033e0832c40a7ca83a75213013d
Jigar Doshi <50413039+GitJig@users.noreply.github.com>
2023-03-12 23:28:21 +0800

    Update oci_workrequest_status.sh
    Commented this line .   Primary mode is user input on command line.  Hard code only if required.
    export WorkReqOCID=<REPLACE_WORKREQUESTOCID_HERE>

--- oci_workrequest_status.sh
+++ oci_workrequest_status.sh
@@ -1,6 +1,7 @@
  #Tested with OCI Cloud Shell
  #This will display status of workrequest specified by WorkReqOCID variable
- export WorkReqOCID=<REPLACE_WORKREQUESTOCID_HERE>
+ #Uncomment next line to provide actual work request OCID instead of providing via command line
+ #export WorkReqOCID=<REPLACE_WORKREQUESTOCID_HERE>
  if [ -z $1 ]
  then
  oci work-requests work-request get --work-request-id $WorkReqOCID
