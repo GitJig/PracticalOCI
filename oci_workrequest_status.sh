6af8f0e07bafbe66f0ecdf9ac3ffb615a7f62308
Jigar Doshi <50413039+GitJig@users.noreply.github.com>
2023-03-12 23:23:32 +0800

    Update oci_workrequest_status.sh
    Check for command line parameter input using $1

--- oci_workrequest_status.sh
+++ oci_workrequest_status.sh
@@ -1,4 +1,9 @@
- #Tested in OCI Cloud Shell
+ #Tested with OCI Cloud Shell
  #This will display status of workrequest specified by WorkReqOCID variable
  export WorkReqOCID=<REPLACE_WORKREQUESTOCID_HERE>
+ if [ -z $1 ]
+ then
  oci work-requests work-request get --work-request-id $WorkReqOCID
+ else
+ oci work-requests work-request get --work-request-id $1
+ fi
