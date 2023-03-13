2c74293f6610cd8c27587f65885856bf6d33bccd
Jigar Doshi <50413039+GitJig@users.noreply.github.com>
2023-03-13 20:59:15 +0800

    Update adb_cross_region_create.sh
    Removed hard coded values

--- adb_cross_region_create.sh
+++ adb_cross_region_create.sh
@@ -1,33 +1,33 @@
 #List all Autonomous DB name and OCID in compartment
-export compname=JDBlog
+export compname=<INSERT_PRIMARY_COMPARTMENT_NAME>
 export comp_id=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
 oci db autonomous-database list --compartment-id $comp_id --all --query "data[].[id,\"display-name\"]" --output table
 
 #save OCID in variable based on name(case sensitive) string
-export ADBName=Calvin
+export ADBName=<INSERT_PRIMARY_DB_NAME>
 export ATPocid=`oci db autonomous-database list --compartment-id $comp_id --query "data[?contains(\"display-name\",'$ADBName')]"|jq -r ".[].id"`
 
 #get required destination standby DB region
-export DestRegion=london
+export DestRegion=<INSERT_STANDBY_CITY_NAME>
 export ociregionname=`oci iam region list --query "data[?contains(\"name\",'$DestRegion')]"|jq -r ".[].name"`
 echo $ociregionname
 
 #get destination region subnet
-export networkcompname=jdoshinetwork
+export networkcompname=<INSERT_TARGET_NETWORK_COMPARTMENT_NAME>
 export ncomp_id=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[]| select(.name == \"${networkcompname}\") | .id"`
 
 #Set remote subnet name
-export remotesubnetname=ODI
+export remotesubnetname=<INSERT_TARGET_SUBNET_NAME>
 #save remote subnetocid
 # Imp - --region flag to query remote region. 
 export remsubnet_ocid=`oci network subnet list --compartment-id $ncomp_id --all --region $ociregionname --query "data[?contains(\"display-name\",'$remotesubnetname')]"|jq -r ".[].id"`
 
 #Uncomment next 3 lines if the target remote region compartment is different from primary DB
-#export compname=jdoshi
+#export compname=<INSERT_TARGET_COMPARTMENT_NAME>
 #export comp_id=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
 #echo $comp_id
 #Create ADB DG
-export standbyDBName=FuzzzyHobbes
+export standbyDBName=<INSERT_STANDBY_DB_DISPLAY_NAME>
 export remoteADBocid=`oci db autonomous-database create-adb-cross-region-data-guard-details  --compartment-id $comp_id --display-name $standbyDBName --source-id $ATPocid --subnet-id $remsubnet_ocid --region $ociregionname|jq -r ".data.id"`
 
 while true
