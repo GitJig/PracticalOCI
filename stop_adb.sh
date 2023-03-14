eb1ecc5da07d278b8b114fd6c12e31a959909a2c
Jigar Doshi <50413039+GitJig@users.noreply.github.com>
2023-03-14 22:10:33 +0800

    Create stop_adb.sh
    separate script for stop ADB

---
+++ stop_adb.sh
@@ -0,0 +1,16 @@
+# Replace <INSERT_COMPARTMENT_NAME> with actual compartment name (case sensitive)
+# Assumes unique compartment name in tenancy. Adjust for duplicate comparment names
+export compname=<INSERT_COMPARTMENT_NAME>
+export Compocid=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
+# Replace <INSERT_DB_NAME> with actual DB name.
+export DBname=<INSERT_DATABASE_NAME>
+# Save DB OCID in variable based on DB name(case sensitive)
+export ATPocid=`oci db autonomous-database list --compartment-id $Compocid --query "data[?contains(\"display-name\",'$DBname')]"|jq -r ".[].id"`
+export WorkReqOCID=`oci db autonomous-database stop --autonomous-database-id $ATPocid |jq -r '."opc-work-request-id"'`
+while true
+do 
+export status=`oci work-requests work-request get --work-request-id $WorkReqOCID|jq -r ".data.status"`
+echo "DB stop is $status"
+echo "Ctrl-C to exit"
+sleep 10
+done
