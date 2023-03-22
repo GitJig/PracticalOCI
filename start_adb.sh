58df4649f710253955b371251b2a8dd265640774
Jigar Doshi <jigardoshi79@gmail.com>
2023-03-22 11:59:28 +0000

    Updated start script to accept command line arguments from compartment and DB name

--- start_adb.sh
+++ start_adb.sh
@@ -1,16 +1,21 @@
+if [ -z "$1" -o  -z "$2" ]
+then
+    echo "Usage - start_adb.sh <INSERT_COMPARTMENT_NAME> <INSERT_DB_NAME>"
+    exit;
+fi
 # Replace <INSERT_COMPARTMENT_NAME> with actual compartment name (case sensitive)
 # Assumes unique compartment name in tenancy. Adjust for duplicate comparment names
-export compname=<INSERT_COMPARTMENT_NAME>
+export compname=$1
 export Compocid=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
 # Replace <INSERT_DB_NAME> with actual DB name.
-export DBname=<INSERT_DATABASE_NAME>
+export DBname=$2
 # Save DB OCID in variable based on DB name(case sensitive)
 export ATPocid=`oci db autonomous-database list --compartment-id $Compocid --query "data[?contains(\"display-name\",'$DBname')]"|jq -r ".[].id"`
 export WorkReqOCID=`oci db autonomous-database start --autonomous-database-id $ATPocid |jq -r '."opc-work-request-id"'`
 while true
 do 
 export status=`oci work-requests work-request get --work-request-id $WorkReqOCID|jq -r ".data.status"`
-echo "startup status -- $status"
+echo "startup request -- $status"
 echo "Ctrl-C to exit"
 sleep 10
 done
