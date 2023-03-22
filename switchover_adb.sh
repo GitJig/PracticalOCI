5c897f81bcbc22d4576b5a1a93aed2acd54d8825
Jigar Doshi <jigardoshi79@gmail.com>
2023-03-22 15:54:32 +0000

    Added Peer region variable

--- switchover_adb.sh
+++ switchover_adb.sh
@@ -17,6 +17,9 @@ export ATPocid=`oci db autonomous-database list --compartment-id $Compocid --que
 #This assumes only cross region DG setup.  To be adjusted for both local and cross region DG.
 # Get DataGuard Peer OCID
 export Peerdbocid=`oci db autonomous-database get --autonomous-database-id $ATPocid|jq -r '.data."peer-db-ids"[0]'`
+#Get Peer Region from Peer OCID
+export PeerRegion=`echo  $Peerdbocid|cut -d . -f 4`
+
 #Switchover DB 
 echo "Initiating Switchover - Status  update can take few minutes"
 export WorkReqOCID=`oci db autonomous-database switchover --autonomous-database-id $ATPocid --peer-db-id $Peerdbocid |jq -r '."opc-work-request-id"'`
@@ -34,7 +37,7 @@ done
 #Future Todo - Modify to exclude any old request
 while true
 do 
-export percent_complete_status=`oci work-requests work-request list  --compartment-id $Compocid --resource-id $Peerdbocid --region uk-london-1 --query "data[?status!='SUCCEDED'&&\"operation-type\"=='Recreate Autonomous Data Guard standby']"|jq -r '.[]."percent-complete"'`
+export percent_complete_status=`oci work-requests work-request list  --compartment-id $Compocid --resource-id $Peerdbocid --region $PeerRegion --query "data[?status!='SUCCEDED'&&\"operation-type\"=='Recreate Autonomous Data Guard standby']"|jq -r '.[]."percent-complete"'`
 echo "Autonomous Data Guard Standby Recreation %  $percent_complete_status"
 echo "Ctrl-C to exit"
 sleep 10
