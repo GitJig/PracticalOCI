758be040cccda8eccdaf6bc8db1a777ca069208f
Jigar Doshi <jigardoshi79@gmail.com>
2023-04-12 08:44:56 +0000

    updated stop script

--- stop_adb.sh
+++ stop_adb.sh
@@ -1,9 +1,31 @@
-# Replace <INSERT_COMPARTMENT_NAME> with actual compartment name (case sensitive)
 # Assumes unique compartment name in tenancy. Adjust for duplicate compartment names
-export compname=<INSERT_COMPARTMENT_NAME>
+if [ $# -lt 2 ]
+then
+	echo "Usage: stop_adb.sh <Compartment Name> <DatabaseName>"
+	exit
+fi
+export compname=$1
 export Compocid=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
-# Replace <INSERT_DB_NAME> with actual DB name.
-export DBname=<INSERT_DATABASE_NAME>
+
+if [ -z $Compocid ]
+then
+	echo "Compartment $compname not found"
+	echo "Exiting"
+	exit
+fi
+
+export DBname=$2
+
+
+export ATPocid=`oci db autonomous-database list --compartment-id $Compocid --query "data[?contains(\"display-name\",'$DBname')]"|jq -r ".[].id"`
+
+if [ -z $ATPocid ]
+then
+	echo "Database $DBname not found"
+	echo "Exiting"
+	exit
+fi
+
 # Save DB OCID in variable based on DB name(case sensitive)
 export ATPocid=`oci db autonomous-database list --compartment-id $Compocid --query "data[?contains(\"display-name\",'$DBname')]"|jq -r ".[].id"`
 export WorkReqOCID=`oci db autonomous-database stop --autonomous-database-id $ATPocid |jq -r '."opc-work-request-id"'`
@@ -12,5 +34,5 @@ do
 export status=`oci work-requests work-request get --work-request-id $WorkReqOCID|jq -r ".data.status"`
 echo "DB stop is $status"
 echo "Ctrl-C to exit"
-sleep 10
+sleep 30
 done
